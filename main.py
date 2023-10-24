5a00119175fa06e71951d334d701cc22d5908b65
Daniela Moreno <dani9292@hotmail.com>
2023-10-24 11:18:38 -0500

    write function

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ content = r.json()
 
 articles = content['articles']
 
-for article in articles:
-    print('TITLE\n', article['title'], '\nDESCRIPTION\n', article['description'])
+# for article in articles:
+# print('TITLE\n', article['title'], '\nDESCRIPTION\n', article['description'])
 
 def get_news(topic, from_date, to_date, language='en', api_key='bd360e94249945b69f6a1e22c16a50cb'):
     url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language={language}&apiKey={api_key}'
+    r = requests.get(url)
+    content = r.json()
+    results = []
+    for article in articles:
+        results.append(f"TITLE\n', {article['title']}, '\nDESCRIPTION\n', {article['description']}")
+    return results
+
+
+print(get_news(topic='space', from_date='2023-10-01', to_date='2023-10-10'))
