c9b9a9eb814af15e75a2263e8cdcde7fd717ab38
Daniela Moreno <dani9292@hotmail.com>
2023-10-24 11:39:09 -0500

    function for country news

---
+++ headlines.py
@@ -0,0 +1,21 @@
+
+# -*- coding: utf-8 -*-
+
+import requests
+
+def get_news(country, api_key='bd360e94249945b69f6a1e22c16a50cb'):
+    url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'
+    r = requests.get(url)
+    content = r.json()
+    articles = content['articles']
+    results = []
+    for article in articles:
+        results.append(f"TITLE\n', {article['title']}, '\nDESCRIPTION\n', {article['description']}")
+    return results
+
+
+print(get_news(country='us'))
+
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
